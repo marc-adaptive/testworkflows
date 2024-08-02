e307e4dd66a6e986d069d3fb5a3f24c94dd5e90f
marc-adaptive <marc.pinto@weareadaptive.com>
2024-08-02 10:37:54 -0400

    hotfix

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,7 +3,7 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        System.out.println("Returning success");
+        System.out.println("Returning success 2");
     }
 
     public static void test() {
